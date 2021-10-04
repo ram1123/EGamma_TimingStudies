dd2c10ae6cc77772361535c65c3253d251c55816
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2021-10-04 15:46:07 +0200

    Updated README

---
+++ GetResults.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# @Author: Ram Krishna Sharma
+# @Author Email: ram.krishna.sharma@cern.ch
+# @Date:   2021-07-14
+# @Last Modified by:   Ram Krishna Sharma
+# @Last Modified time: 2021-07-15
+import os
+
+pathToCheck = "/data/timing/output/rasharma/"
+dirToCheck = "CMSSW_11_3_0.20210713"
+
+# AllDirs = os.listdir(pathToCheck)
+
+# for dir_ in AllDirs:
+#     if dirToCheck in dir_:
+#         print("Directory: {}".format(dir_))
+
+command = 'cat '
+for dirpath, dirnames, files in os.walk(pathToCheck):
+    if dirToCheck in dirpath:
+        # print("=================")
+        # print('Found directory: {}'.format(dirpath))
+        for files_ in files:
+            if files_.endswith('.csv'):
+                num_lines = sum(1 for line in open(os.path.join(dirpath, files_)))
+                if num_lines == 4:
+                    # print(files_)
+                    # os.system('cat '+os.path.join(dirpath,files_))
+                    command = command + " " + os.path.join(dirpath, files_)
+
+os.system(command + " > Individual_Path_timings.csv")
+
+command = 'cat '
+dict_files = {}
+for dirpath, dirnames, files in os.walk(pathToCheck):
+    if dirToCheck in dirpath:
+        # print("=================")
+        # print('Found directory: {}'.format(dirpath))
+        for files_ in files:
+            if files_.endswith('.csv'):
+                num_lines = sum(1 for line in open(os.path.join(dirpath, files_)))
+                if num_lines == 4:
+                    for line in open(os.path.join(dirpath, files_)):
+                        if ""
+                if num_lines != 4:
+                    # print(files_)
+                    dict_files[num_lines] = os.path.join(dirpath, files_)
+                    # os.system('cat '+os.path.join(dirpath,files_))
+                    command = command + " " + os.path.join(dirpath, files_)
+
+os.system(command + " > Many_PathTogether_timings.csv")
+
+command = 'cat '
+for num, files in dict_files.items():
+    print(num, files)
+    command = command + " " + files
+
+os.system(command + " > Many_PathTogether_timings_ordered.csv")
--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # EGamma_TimingStudies
+
 Timing studies of EGamma paths
 
+```bash
+cd /afs/cern.ch/user/r/rasharma/work/EGamma-POG/HLT_tasks/timingStudy
+cmsrel CMSSW_11_3_0
+cd CMSSW_11_3_0/src
+cmsenv
+# Download the HLT Configuration
+# for one path `HLT_Ele28_WPTight_Gsf_v1`
+hltGetConfiguration /dev/CMSSW_11_3_0/GRun --globaltag auto:run3_data_GRun --data --process TIMING --full --offline --max-events 100000 --output none --timing --paths HLTriggerFirstPath,HLTriggerFinalPath,HLT_Ele28_WPTight_Gsf_v1 > hlt_config.py
+ssh vocms007
+cd /afs/cern.ch/user/r/rasharma/work/EGamma-POG/HLT_tasks/timingStudy/CMSSW_11_3_0/src
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cmsenv
+# Submit the jobs
+python3 /data/timing/scripts/timing/submit.py hlt_config.py
+# monitor the jobs
+python3 /data/timing/scripts/timing/job_manager.py
+# Output files can be find here:
+cd /data/timing/output/rasharma
+```
